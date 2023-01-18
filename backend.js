f3d578d1c0e523d1285fce1241a2084f39b6bce3
alyssa-higdon <81454612+alyssa-higdon@users.noreply.github.com>
2023-01-18 00:23:12 -0800

    part 6 working

--- backend.js
+++ backend.js
@@ -65,6 +65,17 @@ app.get('/users/:id', (req, res) => {
 
 });
 
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.status(200).end();
+});
+
+function addUser(user){
+    users['users_list'].push(user);
+}
+
+
 function findUserById(id){
     return users['users_list'].find((user) => user['id'] === id);
     // ^ or return users['users_list'].filter((user) => user['id'] === id);
