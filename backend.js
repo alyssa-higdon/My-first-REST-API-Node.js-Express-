cd76cb6f60baf0039187b352d8e158f67309b548
alyssa-higdon <81454612+alyssa-higdon@users.noreply.github.com>
2023-01-17 22:57:51 -0800

    part 4

--- backend.js
+++ backend.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-const hi = "hi";
 const users = { 
     users_list :
     [
@@ -42,6 +41,21 @@ app.get('/', (req, res) => {
     res.send(users);
 });
 
+app.get('/users', (req, res) => {
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
+});
+
+const findUserByName = (name) => { 
+    return users['users_list'].filter( (user) => user['name'] === name); 
+}
 
 
 app.listen(port, () => {
